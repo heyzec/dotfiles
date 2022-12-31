ee216f55dfb03f7311fe3932a757e9f3ea4890ed
heyzec <hz.tiang@gmail.com>
2023-01-01 02:45:43 +0800

    desktop: add custom icon pack for polybar

--- desktop/install.sh
+++ desktop/install.sh
@@ -1,5 +1,12 @@
 cd "$(dirname "$0")"
 
+if ! fc-match 'heyzec\-polybar\-icon\-fonts:style=Regular' | grep 'heyzec-polybar-icon-fonts'; then
+    mkdir ~/.local/share/fonts
+    cp polybar/heyzec-polybar-icons.ttf ~/.local/share/fonts/
+fi
+
+
+
 # Note to fix: if ~/.config/i3 folder exists, the symlink is created in that folder. this breaks paths
 ln -s -T -fi $(realpath i3/) ~/.config/i3
 ln -s -T -fi $(realpath polybar/) ~/.config/polybar
