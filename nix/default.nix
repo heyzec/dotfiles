08d4f62594f238f96c3fe29b6c1ab505218e7905
heyzec <hz.tiang@gmail.com>
2023-12-23 00:13:11 +0800

    nix(keyd): remove overlay, make config standalone
    Flake lock file updates:
    
    • Updated input 'home-manager':
        'github:nix-community/home-manager/d9297efd3a1c3ebb9027dc68f9da0ac002ae94db' (2023-12-12)
      → 'github:nix-community/home-manager/fb5ac0c870a1b3ffea70e02ab1720d991ce812ae' (2023-12-21)
    • Updated input 'nixpkgs':
        'github:nixos/nixpkgs/a9bf124c46ef298113270b1f84a164865987a91c' (2023-12-11)
      → 'github:nixos/nixpkgs/54aac082a4d9bb5bbc5c4e899603abfb76a3f6d6' (2023-12-19)
    • Updated input 'nixpkgs-stable':
        'github:nixos/nixpkgs/781e2a9797ecf0f146e81425c822dca69fe4a348' (2023-12-10)
      → 'github:nixos/nixpkgs/d65bceaee0fb1e64363f7871bc43dc1c6ecad99f' (2023-12-20)

--- nix/default.nix
+++ nix/default.nix
@@ -4,7 +4,6 @@
     ./hardware/laptop
     ./configuration.nix
     # ./overlays/hyprland.nix
-    ./overlays/keyd.nix
     ./overlays/swhkd.nix
   ];
 }
--- nix/modules/keyd.nix
+++ nix/modules/keyd.nix
@@ -1,49 +1,17 @@
+{ pkgs, ... }:
 {
   services.keyd = {
     enable = true;
     keyboards = {
       default = {
-        ids = [ "*" "-1234:5678" ];
-        settings = {
-          "global" = {
-            # To ignore tap capslock to Esc if held more than 500 ms
-            overload_tap_timeout = 300;
-          };
-          "main" = {
-            # Swap leftalt and meta
-            leftalt = "layer(meta)";
-            meta = "layer(alt)";
-
-            # Capslock is magical
-            capslock = "overload(capslock, esc)";
-
-            # Practise not using arrow keys
-            left = "noop";
-            down = "noop";
-            up = "noop";
-            right = "noop";
-          };
-          "capslock" = {
-            h = "left";
-            j = "down";
-            k = "up";
-            l = "right";
-
-            a = "end";
-            i = "home";
-          };
-          "capslock+shift" = {
-            h = "C-left";
-            j = "pagedown";
-            k = "pageup";
-            l = "C-right";
-
-            # j = "pagedown";
-            # k = "pageup";
-            # l = "end";
-          };
-        };
+        ids = [ ];
+        # My config is added to /etc/keyd/ manually instead of via nix
       };
     };
   };
+  environment.systemPackages = with pkgs; [
+    # Add keyd to programs too for manpage
+    keyd
+  ];
 }
+
