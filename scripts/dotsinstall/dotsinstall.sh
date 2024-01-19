de13a438bb0f1d3ad2ceefeb220c6a0d0feb3a71
heyzec <hz.tiang@gmail.com>
2024-01-19 21:21:13 +0800

    scripts(dotsmenu+dotsinstall): use /bin/sh

--- scripts/dotsinstall/dotsinstall.sh
+++ scripts/dotsinstall/dotsinstall.sh
@@ -1,45 +1,56 @@
-#!/usr/bin/env bash
+#!/bin/sh
+#
+
+# Exit immediately if any command fails
+set -e
+
+# Exit immediately if any command fails
+set -e
 
 directory=$(dirname "$(realpath "$0")")
 ITEMS_FILE="$directory/items.ini"
-
-subst() {
-    # In case envsubst is not available, use sed
-    if $(command -v envsubst); then
-        envsubst
-    else
-        sed "s/\$HOME/${HOME//\//\\/}/"
-    fi
-}
-
-declare -A table
-declare -a sections=()
-
-section_i=0
-while IFS= read -r line; do
-    if [[ $line =~ ^\[([a-z-]+)\]$ ]]; then
-        section=${BASH_REMATCH[1]}
-
-        sections[${#sections[@]}]=$section
-
-        section_i=$((section_i + 1))
-    else
-        [[ $line =~ ^\[([a-z]+)\]$ ]]
-        IFS="=" read -a temp <<<"$line"
-        key=${temp[0]}
-        value=${temp[1]}
-        table[$section,$key]=$value
-    fi
-done < $ITEMS_FILE
 
 if [ $# -ne 1 ]; then
     echo "usage: ./install <program>"
     exit
 fi
 
-selected="$1"
-src=${table[$selected,src]}
-dest=${table[$selected,dest]}
+subst() {
+    # In case envsubst is not available, use sed
+    if command -v envsubst &> /dev/null; then
+        envsubst
+    else
+        # The innermost sed is to escape slashes in path for outer sed command to be valid
+        sed "s/\$HOME/$(echo "$HOME" | sed 's:/:\\\/:g')/"
+    fi
+}
+
+extract_section() {
+    section_name="$1"
+    filename="$2"
+    cat "$filename" | awk '
+        BEGIN{state=0}
+        /'"$section_name"'/&&state==0{state++;next}
+        state==1&&/\[[a-z-]+\]/{exit;}
+        state==1{print $0}
+    ' | sed '/^$/d'
+}
+
+extract_value() {
+    key="$1"
+    key_value_pairs="$2"
+    echo "$key_value_pairs" | grep "^$key=" | sed "s/^$key=//"
+}
+
+program="$1"
+
+# Extract relevant section from config file
+key_value_pairs=$(extract_section "$program" "$ITEMS_FILE")
+
+# Extract values
+src=$(extract_value src "$key_value_pairs")
+dest=$(extract_value dest "$key_value_pairs")
+
 
 if [ -z "$src" ]; then
     echo "No valid entry selected!"
@@ -47,7 +58,7 @@ if [ -z "$src" ]; then
 fi
 
 src_expanded=$(realpath "$src")
-dest_expanded=$(subst <<< "$dest")
+dest_expanded=$(echo "$dest" | subst)
 
 set -x
 ln --symbolic --no-target-directory --verbose "$src_expanded" "$dest_expanded"
--- scripts/dotsmenu/dotsmenu.sh
+++ scripts/dotsmenu/dotsmenu.sh
@@ -1,87 +1,112 @@
-#!/usr/bin/env bash
+#!/bin/sh
+# dotsmenu.sh
+
+# Exit immediately if any command fails
+set -e
 
 directory=$(dirname "$(realpath "$0")")
 ITEMS_FILE="$directory/items.ini"
-
 terminal="${TERMINAL:=foot}"  # default to foot
+
+log() {
+    notify-send "dotsmenu" "$@"
+}
 
 subst() {
     # In case envsubst is not available, use sed
-    if $(command -v envsubst); then
+    if command -v envsubst &> /dev/null; then
         envsubst
     else
-        sed "s/\$HOME/${HOME//\//\\/}/"
+        # The innermost sed is to escape slashes in path for outer sed command to be valid
+        sed "s/\$HOME/$(echo "$HOME" | sed 's:/:\\\/:g')/"
     fi
 }
 
-declare -A table
-declare -a sections=()
+parse_table_from_ini() {
+    # Unused function: bash example to parse a associative array from ini
+    declare -A table
+    declare -a sections=()
 
-section_i=0
-while IFS= read -r line; do
-    if [[ $line =~ ^\[([a-z-]+)\]$ ]]; then
-        section=${BASH_REMATCH[1]}
+    section_i=0
+    while IFS= read -r line; do
+        if [[ $line =~ ^\[([a-z-]+)\]$ ]]; then
+            section=${BASH_REMATCH[1]}
+            sections[${#sections[@]}]=$section
+            section_i=$((section_i + 1))
+        else
+            [[ $line =~ ^\[([a-z]+)\]$ ]]
+            IFS="=" read -a temp <<<"$line"
+            key=${temp[0]}
+            value=${temp[1]}
+            table[$section,$key]=$value
+        fi
+    done < "$ITEMS_FILE"
+}
 
-        sections[${#sections[@]}]=$section
+parse_sections() {
+    filename="$1"
+    # Note: BSD grep does not GNU grep's -P flag
+    cat "$filename" | grep '^\[[a-z]\+\]$' | sed -E 's/\[|\]//g'
+}
 
-        section_i=$((section_i + 1))
-    else
-        [[ $line =~ ^\[([a-z]+)\]$ ]]
-        IFS="=" read -a temp <<<"$line"
-        key=${temp[0]}
-        value=${temp[1]}
-        table[$section,$key]=$value
-    fi
-done < "$ITEMS_FILE"
+extract_section() {
+    section_name="$1"
+    filename="$2"
+    cat "$filename" | awk '
+        BEGIN{state=0}
+        /'"$section_name"'/&&state==0{state++;next}
+        state==1&&/\[[a-z-]+\]/{exit;}
+        state==1{print $0}
+    ' | sed '/^$/d'
+}
 
-bar="$(printf "\n%s" "${sections[@]}")"
-config_names="${bar:1}"
-selected_config_name=$( (echo "$config_names") | rofi -dmenu -p "Select config file")
-selected_config_file=${table[$selected_config_name,file]}
-selected_config_file_expanded=$(subst <<< "$selected_config_file")
+extract_value() {
+    key="$1"
+    key_value_pairs="$2"
+    echo "$key_value_pairs" | grep "^$key=" | sed "s/^$key=//"
+}
 
+
+# Parse section headers in config file
+all_sections="$(parse_sections "$ITEMS_FILE")"
+
+# Let user select which config
+selected_config_name=$( (echo "$all_sections") | rofi -dmenu -p "Select config file")
+
+# Extract relevant section from config file
+key_value_pairs=$(extract_section "$selected_config_name" "$ITEMS_FILE")
+
+# Extract values
+file=$(extract_value file "$key_value_pairs")
+sudo=$(extract_value sudo "$key_value_pairs")
+posthook=$(extract_value posthook "$key_value_pairs")
+cd=$(extract_value cd "$key_value_pairs")
+
+selected_config_file_expanded=$(echo "$file" | subst)
 if [ -z "$selected_config_file_expanded" ]; then
-    notify-send "dotsmenu" "No valid entry selected!"
+    log "No valid entry selected!"
     exit
 fi
 
 if ! [ -e "$selected_config_file_expanded" ]; then
-    notify-send "dotsmenu" "Path to $selected_config_name ($selected_config_file_expanded) does not exist!"
+    log "Path to $selected_config_name ($selected_config_file_expanded) does not exist!"
 fi
 
-if [ "${table[$selected_config_name,sudo]}" = "true" ]; then
+if [ "$sudo" = "true" ]; then
     editor="sudoedit"
 else
     editor="$EDITOR"
 fi
 
-
-# if ! [ -z "${table[$selected_config_name,posthook]}" ]; then
-#     cmd=${table[$selected_config_name,posthook]}
-#     export cmd
-#     func() {
-#         # echo yay running posthook
-#         $cmd
-#     }
-# else
-#     func() { :; }
-# fi
-# export -f func
-# kitty --hold bash -c "$editor $selected_config_file_expanded; func"
-
-
-if [ -n "${table[$selected_config_name,posthook]}" ]; then
-    cmd=${table[$selected_config_name,posthook]}
+if [ -n "$posthook" ]; then
+    cmd="$posthook"
 else
     cmd=':'
 fi
 
-#magic="$editor $selected_config_file_expanded || exit; $cmd || bash"
-#while nvim test || exit; do false && break || (echo "Press enter to edit"; read); done
 magic="while $editor $selected_config_file_expanded || exit; do ($cmd) && break || (echo 'Command failed, press enter to edit'; read); done"
-if [ -n "${table[$selected_config_name,cd]}" ]; then
-    magic="cd ${table[$selected_config_name,cd]}; $magic"
+if [ -n "$cd" ]; then
+    magic="cd $cd; $magic"
 fi
-# bash -o pipefail -c "$magic"
 $terminal -e bash -o pipefail -c "$magic"
 
