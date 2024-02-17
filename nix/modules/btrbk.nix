904da6f5866b93fc36de14233e8eb4fafbb784ca
heyzec <61238538+heyzec@users.noreply.github.com>
2024-02-17 18:16:14 +0800

    nix(btrfs): update config for scheduled backups

--- nix/modules/btrbk.nix
+++ nix/modules/btrbk.nix
@@ -1,10 +1,10 @@
 {
     services.btrbk = {
-      # Name instance to be "btrbk" to generate config at default location.
-      instances."btrbk" = {
+      # Note: To generate config at default location, name instance to be "btrbk" instead
+
+      instances."data" = {
         onCalendar = "*-*-* 22:00:00";
         settings = {
-        # ssh_identity = "/home/heyzec/.ssh/id_ed25519";
         ssh_user = "btrbk";
           volume."/mnt/D" = {
             snapshot_preserve_min = "latest";
@@ -13,6 +13,20 @@
             subvolume = "@data";
             snapshot_dir = "@snapshots";
             target = "ssh://heyzec.mooo.com:40478/media/backups/btrbk";
+          };
+        };
+      };
+      instances."home" = {
+        onCalendar = "*-*-* 22:00:00";
+        settings = {
+        ssh_user = "btrbk";
+          volume."/mnt/root" = {
+            snapshot_preserve_min = "latest";
+            snapshot_preserve = "7d 4w 1m";
+
+            subvolume = "@home";
+            snapshot_dir = "/mnt/root/@home/.snapshots/";
+            target = "ssh://heyzec.mooo.com:40478/media/backups/btrbk/home";
           };
         };
       };
