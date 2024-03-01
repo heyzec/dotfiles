57dbb5c807ff5e15ad31d356ed9abf409e441b45
heyzec <61238538+heyzec@users.noreply.github.com>
2024-03-01 21:59:37 +0800

    big nonnix cleanup, need review

--- scripts/wallpaper/setwallpaper.sh
+++ scripts/wallpaper/setwallpaper.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+# Change to get wallpaper?, so swaybg is within hyprland?
 swaybg --image "$HOME/Pictures/Wallpapers/wallpaper" --mode fill
+# This is blocking!!!!
 flavours generate dark ~/Pictures/Wallpapers/wallpaper
 flavours apply generated
