42c5384cd823ad7441b0a0112d8351f65fa1055b
heyzec <61238538+heyzec@users.noreply.github.com>
2024-03-25 23:06:47 +0800

    nix(btrbk): update config for scheduled backups

--- nix/modules/btrbk.nix
+++ nix/modules/btrbk.nix
@@ -1,35 +1,42 @@
 {
-    services.btrbk = {
-      # Note: To generate config at default location, name instance to be "btrbk" instead
-
-      instances."data" = {
-        onCalendar = "*-*-* 22:00:00";
-        settings = {
+  # Note: To generate config at default location, name instance to be "btrbk" instead
+  services.btrbk.instances = {
+    "data" = {
+      onCalendar = "*-*-* 22:00:00";
+      settings = {
+        ssh_identity = "/etc/btrbk/btrbk";
         ssh_user = "btrbk";
-          volume."/mnt/D" = {
-            snapshot_preserve_min = "latest";
-            snapshot_preserve = "7d 4w 1m";
+        volume."/mnt/D" = {
+          subvolume = "@data";
 
-            subvolume = "@data";
-            snapshot_dir = "@snapshots";
-            target = "ssh://heyzec.mooo.com:40478/media/backups/btrbk";
-          };
-        };
-      };
-      instances."home" = {
-        onCalendar = "*-*-* 22:00:00";
-        settings = {
-        ssh_user = "btrbk";
-          volume."/mnt/root" = {
-            snapshot_preserve_min = "latest";
-            snapshot_preserve = "7d 4w 1m";
+          snapshot_preserve_min = "latest";
+          snapshot_preserve = "7d 4w 1m";
+          snapshot_dir = "@snapshots";
 
-            subvolume = "@home";
-            snapshot_dir = "/mnt/root/@home/.snapshots/";
-            target = "ssh://heyzec.mooo.com:40478/media/backups/btrbk/home";
-          };
+          target_preserve_min = "latest";
+          target_preserve = "7d 4w 1m";
+          target = "ssh://heyzec.mooo.com:40478/media/backups/btrbk/data";
         };
       };
     };
+    "home" = {
+      onCalendar = "*-*-* 22:00:00";
+      settings = {
+        ssh_identity = "/etc/btrbk/btrbk";
+        ssh_user = "btrbk";
+        volume."/mnt/root" = {
+          subvolume = "@home";
+
+          snapshot_preserve_min = "latest";
+          snapshot_preserve = "7d 4w 1m";
+          snapshot_dir = "/mnt/root/@home/.snapshots/";
+
+          target_preserve_min = "latest";
+          target_preserve = "7d 4w 1m";
+          target = "ssh://heyzec.mooo.com:40478/media/backups/btrbk/home";
+        };
+      };
+    };
+  };
 }
 
