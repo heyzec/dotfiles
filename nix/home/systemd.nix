d8aa44072731812f99be84f3f7f83a00c6d63abb
heyzec <61238538+heyzec@users.noreply.github.com>
2024-05-01 18:37:08 +0800

    nix(lib): create timeron wrapper

--- nix/home/systemd.nix
+++ nix/home/systemd.nix
@@ -1,34 +1,5 @@
 { pkgs, ... }:
 {
-  systemd.user.services = {
-    battery_status = {
-      Unit = {
-        Description = "low battery notification service";
-      };
-      Service = {
-        Type = "oneshot";
-        ExecStart = toString (
-          pkgs.writeShellScript "battery-status-script" ''
-            set -eou pipefail
-            ${pkgs.bash}/bin/bash -c notify-send hi hi;
-          ''
-        );
-      };
-      Install.WantedBy = [ "default.target" ];
-    };
-  };
-
-  systemd.user.timers = {
-    battery_status = {
-      Unit.Description = "timer for battery_status service";
-      Timer = {
-        Unit = "battery_status";
-    OnBootSec = "1m";
-        OnUnitActiveSec = "1m";
-      };
-      Install.WantedBy = [ "timers.target" ];
-    };
-  };
   systemd.user.services."polkit-agent" = {
     Unit = {
       Description = "polkit-gnome-authentication-agent-1";
