6903ba13ced7c986ca70d26bc10ac583b455807a
heyzec <61238538+heyzec@users.noreply.github.com>
2024-07-02 18:29:17 +0800

    add irrelevant

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -64,8 +64,8 @@ return {
                             cmp.select_next_item()
                         elseif luasnip.expand_or_jumpable() then
                             luasnip.expand_or_jump()
-                        elseif has_words_before() then
-                            cmp.complete()
+                        -- elseif has_words_before() then
+                            -- cmp.complete()
                         else
                             fallback()
                         end
