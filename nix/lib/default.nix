9d29f7af4ae3ffeec11d9155fd9a443363433043
heyzec <61238538+heyzec@users.noreply.github.com>
2024-07-05 00:51:16 +0800

    nix: run AutoArchiver using timeron wrapper lib

--- nix/lib/default.nix
+++ nix/lib/default.nix
@@ -1,6 +1,7 @@
-{ lib, ... }:
+{ pkgs, lib, ... }:
 {
   umport = import ./umport.nix { inherit lib; };
   overrideNixpkgs = import ./overrideNixpkgs.nix;
   overrideSrc = import ./overrideSrc.nix;
+  timeron = import ./timeron.nix { inherit pkgs lib; };
 }
