0e69db4284cdcddacc8dac4b0c9fbf0e06efbe69
heyzec <61238538+heyzec@users.noreply.github.com>
2024-08-13 21:12:14 +0800

    nvim

--- nvim/lua/init.lua
+++ nvim/lua/init.lua
@@ -22,3 +22,14 @@ vim.api.nvim_create_autocmd('ModeChanged', {
     desc = 'Enable diagnostics when leaving insert mode',
     callback = function(e) vim.diagnostic.enable(e.buf) end
 })
+
+vim.diagnostic.config({
+    signs = {
+        text = {
+            [vim.diagnostic.severity.ERROR] = '',
+            [vim.diagnostic.severity.WARN] = '',
+            [vim.diagnostic.severity.INFO] = '',
+            [vim.diagnostic.severity.HINT] = '',
+        },
+    },
+})
