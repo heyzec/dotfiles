7cce22dae3ab0acce75729ca1397c2568100d63b
heyzec <61238538+heyzec@users.noreply.github.com>
2024-08-13 21:12:14 +0800

    nvim

---
+++ nvim/lua/plugins/nvim-ts-autotag.lua
@@ -0,0 +1,7 @@
+-- Use treesitter to auto close and auto rename html tag
+-- Treesitter parser for HTML is required: :TSInstall html
+-- Note that luasnip also provides such common HTML snippets
+return {
+    "windwp/nvim-ts-autotag",
+    opts = {},
+}
