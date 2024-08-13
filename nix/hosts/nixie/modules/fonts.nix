24b8d7ed2e0e60362fcefba4d5f806887780c70f
heyzec <61238538+heyzec@users.noreply.github.com>
2024-08-13 21:27:44 +0800

    nix: update fonts used

--- nix/hosts/nixie/modules/fonts.nix
+++ nix/hosts/nixie/modules/fonts.nix
@@ -1,25 +1,11 @@
-################################################################################
-##### Fonts
-################################################################################
+# System-wide fonts, prefer setting fonts in home-manager
 { pkgs, ... }:
-
 {
   fonts = {
     packages = with pkgs; [
       roboto             # Google's sans-serif fonts
       roboto-mono        # Google's sans-serif monospace font
-      noto-fonts         # Google's fonts
-      noto-fonts-emoji   # Google's open-source Emoji 14.0
-
-      # Only install a selection of fonts from nerdfonts repository
-      (nerdfonts.override { fonts = [ "RobotoMono" ]; })
+      vistafonts
     ];
-    fontconfig.enable = true;
-    fontconfig.defaultFonts = {
-      serif = [ "Noto Serif" ];
-      sansSerif = [ "Noto Sans" ];
-      monospace = [ "Noto Sans Mono" ];
-      # also set monospace and emoji
-    };
   };
 }
