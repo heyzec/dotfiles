f70e57236511fb7a2681262257ca9343d1f25a5f
heyzec <61238538+heyzec@users.noreply.github.com>
2024-08-17 09:37:42 +0800

    vscode, nvim: add inline git blame text

--- nvim/lua/plugins/git-blame-nvim.lua
+++ nvim/lua/plugins/git-blame-nvim.lua
@@ -1,3 +1,9 @@
 return {
     'f-person/git-blame.nvim',
+    opts = {
+        -- message with a margin of 2 spaces
+        message_template = "  <author> (<date>) • <summary>",
+        date_format = "%r",  -- use relative date
+        delay = 250,
+    },
 }
