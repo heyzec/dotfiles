ee0364269f16b68cd9e5845884079f416737a000
heyzec <61238538+heyzec@users.noreply.github.com>
2024-09-15 21:20:47 +0800

    Revert "nvim: ChatGPT, gen.nvim, Codeium"
    This reverts commit 2b5d8a937a9fb7e70437c8ff3c9fc21f0b582dc2.

--- nvim/lua/plugins/gen-nvim.lua
+++
@@ -1,7 +0,0 @@
-return {
-    "David-Kunz/gen.nvim",
-    config = function ()
-        vim.keymap.set('v', '<leader>]', ':Gen<CR>')
-        vim.keymap.set('n', '<leader>]', ':Gen<CR>')
-    end,
-}
