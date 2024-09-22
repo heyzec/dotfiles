037141cf7a6593422b4ab7b1b3428a1d247e8064
heyzec <61238538+heyzec@users.noreply.github.com>
2024-09-23 01:44:02 +0800

    home: create a file server for SA

--- nix/hosts/homelab/modules/caddy.nix
+++ nix/hosts/homelab/modules/caddy.nix
@@ -1,6 +1,36 @@
 {
   services.caddy = {
     enable = true;
+
+    # Uncomment this block for debug mode. Note that line "debug" will not work in NixOS unlike in Caddyfile
+    # logFormat = ''
+    #   level DEBUG
+    # '';
+
+    # TODO: Wildcard subdomain requires DNS challenge and setting up to DeSEC
+    # virtualHosts."*.app.heyzec.dedyn.io".extraConfig = ''
+    #   @sa host sa-vscode-frontend.app.heyzec.dedyn.io
+    #
+    #   handle @sa {
+    #       header {
+    #         Access-Control-Allow-Origin *
+    #       }
+    #       try_files /var/www/html /index.html
+    #   }
+    #
+    #   handle {
+    #       abort
+    #   }
+    # '';
+
+    virtualHosts."sa-vscode-frontend.heyzec.dedyn.io".extraConfig = ''
+      header {
+        Access-Control-Allow-Origin *
+      }
+      file_server
+      root * /var/www/html
+      try_files {path} /index.html
+    '';
   };
   networking.firewall.allowedTCPPorts = [80 443];
 
