757913af48ae57f19ab53fb27e1dc34c7df826d2
heyzec <61238538+heyzec@users.noreply.github.com>
2024-10-01 20:47:11 +0800

    comment out nvim env cuz of flake update

--- nix/modules/neovim.nix
+++ nix/modules/neovim.nix
@@ -21,20 +21,21 @@
 in {
   programs = {
     neovim = {
-      package = pkgs.symlinkJoin {
-        name = "neovim-unwrapped-wrapped";
-        paths = [ pkgs.neovim-unwrapped ];
-        nativeBuildInputs = [ pkgs.makeWrapper ];
-        # Using lower-level makeShellWrapper instead of wrapProgram
-        # The vim-tmux-navigator plugin will not work when wrapProgram changes the command to .nvim-wrapped
-        # See https://github.com/NixOS/nixpkgs/blob/master/pkgs/build-support/setup-hooks/make-wrapper.sh, look for `hidden` variable
-        # See https://github.com/christoomey/vim-tmux-navigator/blob/master/vim-tmux-navigator.tmux, look for `is-vim` variable
-        postBuild = ''
-          makeShellWrapper "${pkgs.neovim-unwrapped}/bin/nvim" "$out/bin/nvim-temp" \
-            --prefix PATH : ${pkgs.lib.makeBinPath extraPackages}
-          mv "$out/bin/nvim-temp" "$out/bin/nvim"
-        '';
-      };
+      package = pkgs.neovim-unwrapped;
+      # package = pkgs.symlinkJoin {
+      #   name = "neovim-unwrapped-wrapped";
+      #   paths = [ pkgs.neovim-unwrapped ];
+      #   nativeBuildInputs = [ pkgs.makeWrapper ];
+      #   # Using lower-level makeShellWrapper instead of wrapProgram
+      #   # The vim-tmux-navigator plugin will not work when wrapProgram changes the command to .nvim-wrapped
+      #   # See https://github.com/NixOS/nixpkgs/blob/master/pkgs/build-support/setup-hooks/make-wrapper.sh, look for `hidden` variable
+      #   # See https://github.com/christoomey/vim-tmux-navigator/blob/master/vim-tmux-navigator.tmux, look for `is-vim` variable
+      #   postBuild = ''
+      #     makeShellWrapper "${pkgs.neovim-unwrapped}/bin/nvim" "$out/bin/nvim-temp" \
+      #       --prefix PATH : ${pkgs.lib.makeBinPath extraPackages}
+      #     mv "$out/bin/nvim-temp" "$out/bin/nvim"
+      #   '';
+      # };
       enable = true;
       defaultEditor = true;
 
