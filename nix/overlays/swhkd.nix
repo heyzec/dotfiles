f0125cb3d8c30af6d06e72223bacb6418032efc9
heyzec <61238538+heyzec@users.noreply.github.com>
2024-10-27 01:26:43 +0800

    nix: fix overlay url failure

--- nix/overlays/swhkd.nix
+++ nix/overlays/swhkd.nix
@@ -1,11 +1,63 @@
-{ lib, pkgs, ... }:
 {
+  lib,
+  pkgs,
+  ...
+}: {
   imports = [
     (lib.heyzec.overrideNixpkgs {
       inherit pkgs;
-      url = "https://github.com/mibmo/nixpkgs/archive/21a5faab9cf2779b566abf6ec730d31489f2b749.tar.gz";
-      sha256 = "192m8b8lrq85irlxmdrlzksswb16hbjqc292mh0fmqip3k4zs0qf";
+      url = "https://github.com/mibmo/nixpkgs/archive/03cccb60c220ba25a0aa7be7221204156fe29bc6.tar.gz";
+      sha256 = "17ffvjv65r8zy5vdva3f29r2bbrsm4rmiyl8f5mqsbwkx5g7crw6";
       names = ["swhkd"];
     })
   ];
 }
+# This overlay is based off a closed PR on the Nixpkgs repository.
+# In case the commit is lost, the contents of the PR is included below.
+# From 03cccb60c220ba25a0aa7be7221204156fe29bc6 Mon Sep 17 00:00:00 2001
+# From: mib <mib@mib.dev>
+# Date: Sat, 16 Sep 2023 00:06:07 +0200
+# Subject: [PATCH] swhkd: init at 1.2.2
+#
+# ---
+#  pkgs/by-name/sw/swhkd/package.nix | 30 ++++++++++++++++++++++++++++++
+#  1 file changed, 30 insertions(+)
+#  create mode 100644 pkgs/by-name/sw/swhkd/package.nix
+#
+# diff --git a/pkgs/by-name/sw/swhkd/package.nix b/pkgs/by-name/sw/swhkd/package.nix
+# new file mode 100644
+# index 0000000000000..e3aa86ab51397
+# --- /dev/null
+# +++ b/pkgs/by-name/sw/swhkd/package.nix
+# @@ -0,0 +1,30 @@
+# +{ lib
+# +, fetchFromGitHub
+# +, rustPlatform
+# +}:
+# +
+# +rustPlatform.buildRustPackage rec {
+# +  pname = "swhkd";
+# +  version = "1.2.2";
+# +
+# +  src = fetchFromGitHub {
+# +    owner = "waycrate";
+# +    repo  = "swhkd";
+# +    rev = version;
+# +    hash = "sha256-mpE+//a44wwraCCpBTnWXslLROF2dSIcv/kdpxHLD4M=";
+# +  };
+# +
+# +  cargoHash = "sha256-H14YwK4Ow86UxVXoclCjk2xYtu8L/44zkzf9gpveAh8=";
+# +
+# +  meta = with lib; {
+# +    description = "Display protocol-independent hotkey daemon";
+# +    longDescription = ''
+# +      A display protocol-independent hotkey daemon made in Rust.
+# +      Uses an easy-to-use configuration system inspired by sxhkd, so you can easily add or remove hotkeys.
+# +      Attempts to be a drop-in replacement of sxhkd, meaning your sxhkd config file should be compatible with swhkd.
+# +    '';
+# +    license = licenses.bsd2;
+# +    maintainers = with maintainers; [ mib ];
+# +    platforms = platforms.linux;
+# +  };
+# +}
+
