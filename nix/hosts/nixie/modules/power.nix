8c18a754c0b943addc1245c3234339546864adc0
heyzec <61238538+heyzec@users.noreply.github.com>
2025-03-26 07:42:56 +0800

    update power settings

--- nix/hosts/nixie/modules/power.nix
+++ nix/hosts/nixie/modules/power.nix
@@ -2,9 +2,9 @@
   heyzec.power = {
     enable = true;
 
-    blankAfter = 10 * 60;           # 10 minutes
-    sleepAfter = 15 * 60;           # 15 minutes
-    hibernateAfter = 1 * 60 * 60;   # 1 hour
+    blankAfter = 10 * 60; # 10 minutes
+    sleepAfter = 15 * 60; # 15 minutes
+    hibernateAfter = 1 * 60 * 60; # 4 hours
 
     commandBlankScreen = "hyprctl dispatch dpms off";
     commandUnblankScreen = "hyprctl dispatch dpms on";
