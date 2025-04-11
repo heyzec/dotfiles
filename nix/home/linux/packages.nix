4c0babab08c825eb440a8153f2e7023650643bb3
heyzec <61238538+heyzec@users.noreply.github.com>
2025-04-11 22:29:05 +0800

    nix(PACKAGE): full reorg of packages to follow Arch Application List

--- nix/home/linux/packages.nix
+++ nix/home/linux/packages.nix
@@ -2,41 +2,40 @@
   # https://github.com/NixOS/nixpkgs/issues/216361
   # Packages here can break if nixpkgs-unstable goes out of sync with home manager master
   home.packages = with pkgs; [
-    insomnia    # REST API client
+    # Using https://wiki.archlinux.org/title/List_of_applications as a classification system
 
-    ##### Applications: Communication Utilities
-    telegram-desktop-gtk  # Telegram client (patched to use GTK3 over QT filepicker, see overlays)
-    webcord               # Alternative Discord client
-    whatsapp-for-linux    # unofficial client
-    # zoom              # Zoom client
-    teams-for-linux
-    slack
-
-
-
-    pdfsam-basic           # PDF editing
-    xournalpp        # tablet notetaking software
-    joplin-desktop
-    obisidan
-
-
-    # [Specialised CTF Tools]
-    thc-hydra        # online password-cracking tool
-    john             # John the Ripper, an offline password-cracker
-
-    tectonic
-
-
+    # A.2.5. Documents - Database Tools
     dbeaver-bin
 
-    waypaper
+    # A.6.1/3.2. Documents: PDF and DjVu: Basic editors
+    pdfsam-basic #         # PDF editor with merging, splitting and rotating
 
-    ticktick
-    linux-wifi-hotspot
-    bottles
-    mindustry
+    # A.10.1.2. Documents - Note-taking software (Graphical)
+    joplin-desktop
     obsidian
-    syncthing
+
+    # A.10.1.3. Documents - Note-taking software (Stylus note-taking)
+    xournalpp              # tablet notetaking software
+
+    # B.5.5.9. Internet - Instant messaging clients (Other IM clients)
+    telegram-desktop-gtk # # Telegram client (patched to use GTK3 over QT filepicker, see overlays)
+    whatsapp-for-linux #   # unofficial client
+    webcord #              # Alternative Discord client
+    teams-for-linux
+    zoom
+
+    # F.3. Utilities - Development
+    bruno #                # REST API client
+
+    # F.6.21. Utilities - Compatibility layers
+    bottles
+
+    # G.1.6.2. Other - Task management (Graphical)
+    ticktick
+
+    # UNCATEGORISED
+    linux-wifi-hotspot
+    mindustry
   ];
 }
 
--- nix/hosts/nixie/modules/packages.nix
+++ nix/hosts/nixie/modules/packages.nix
@@ -1,5 +1,9 @@
 { config, pkgs, inputs, ... }: {
   environment.systemPackages = with pkgs; [
+    # TODO: Not to see command-line utils as different as GUI
+    # E.g. btop may be command line, but it is used more like a GUI because it is not
+    # used as part of a script somewhere else in my system
+
     ################################################################################
     ##### Command-line Utilities
     ################################################################################
@@ -9,35 +13,33 @@
 
     ##### Command line utilities: Hardware drivers related #####
     bluez               # bluetooth related commands (e.g. bluetoothctl)
-    brightnessctl       # backlight control
+    brightnessctl       # (F.6.11) backlight control
     ddcutil             # change monitor settings using DDC/CI
-    light               # backlight control
+    light               # (F.6.11) backlight control
     lshw                # list hardware configuration
-    ntfs3g              # for ntfs
     pciutils            # pci related commands (e.g. lspci)
     usbutils            # usb related commands (e.g. lsusb)
     virtiofsd           # additional driver for shared directory virtualisation (VMs)
 
     ##### Command line utilities: Terminal #####
-    fastfetch           # terminal system info display
+    fastfetch           # (F.6.3.1) terminal system info display
 
     ##### Command line utilities: System #####
     btdu                # disk usage profiler for btrfs
-    btop                # better top
+    btop                # (F.6.1.1) better top
 
     ##### Command line utilities: Downloads, Backups, Encryption #####
     git-crypt           # encrypt files in a git repository
     gocryptfs           # file-based encryption as a mountable FUSE filesystem
     (callPackage ../../../packages/gocryptfs-scripts.nix {})
     (python3Packages.callPackage ../../../packages/wasg.nix {})
-    rclone              # manage files on cloud storage
-    restic              # push-based backup tool
-    wireguard-tools     # tools for configuring wireguard, a VPN protocol (e.g. wg, wg-quick)
-    yt-dlp              # download youtube videos
+    rclone              # (B.4.4.1) manage files on cloud storage
+    wireguard-tools     # (B.1.2) tools for configuring wireguard, a VPN protocol (e.g. wg, wg-quick)
+    yt-dlp              # (B.4.1.1) download youtube videos
 
 
     ##### Command line utilities: Others #####
-    imagemagick         # edit image files (e.g. convert)
+    imagemagick         # (C.2.3.1) edit image files (e.g. convert)
     kanshi              # dynamic display configuration tool
     pdfgrep             # grep text within PDFs
 
@@ -57,12 +59,12 @@
     libnotify              # library needed for dunst, a notification daemon
     playerctl              # media control
     rofi-wayland           # application launcher
-    smile                  # emoji picker
+    smile                  # (F.4.1) emoji picker
     swaybg                 # wallpaper setter for wayland
     # wlogout                # logout menu
 
     # flameshot               # screen capture
-    grimblast              # wrapper script for grim (grab an image) and slurp (select a region)
+    grimblast              # (C.2.13/1.1) wrapper script for grim (grab an image) and slurp (select a region)
     swappy                 # Wayland native snapshot editing tool
 
     cliphist               # wayland clipboard manager
@@ -75,7 +77,6 @@
     ##### System trays #####
     indicator-sound-switcher   # sound input/output selector
     networkmanagerapplet       # NetworkManager
-    networkmanager-openvpn     # enable/disable OpenVPN profiles
 
     ##### Theming #####
     lxappearance        # GTK+ theme switcher
@@ -86,27 +87,34 @@
     ##### Applications
     ################################################################################
 
+
+
     ##### Standard desktop environment #####
-    baobab               # disk usage analyzer
-    chromium             # open-source chrome
-    font-manager         # easily manage desktop fonts
-    gnome-text-editor    # simple text editor
+    baobab               # (F.5.5.2) disk usage analyzer
+    chromium             # (B.2.2.2) open-source chrome
+    font-manager         # (F.6.5) easily manage desktop fonts
+    gnome-text-editor    # (A.1.4) simple text editor
     simple-scan          # frontend for SANE
-    gparted              # frontend to parted, a partitioning tool
-    gthumb               # image viewer with simple editing tools
+    gparted              # (F.5.1/3.1.1) frontend to parted, a partitioning tool
+    gthumb               # (C.2.2) image viewer with simple editing tools
     libvncserver         # for remmina to support VNC
-    mate.atril           # simple pdf viewer
+    mate.atril           # (A.6.1/2.2) simple pdf viewer
     overskride           # GUI bluetooth manager
-    qimgv                # simple image viewer
-    remmina              # remote desktop client
-    transmission_4-gtk     # lightweight BitTorrent client
-    vlc                  # video player
+    qimgv                # (C.2.1.3) simple image viewer
+    remmina              # (B.7.1) remote desktop client
+    transmission_4-gtk   # (B.4.6.2) lightweight BitTorrent client
+    vlc                  # (C.4.1.2.3) video player
+
+    # C.2.5 Multimedia - Raster graphics editors
+    gimp             # image editing suite
+    # C.2.8 Multimedia - Vector graphics editors
+    inkscape         # vector graphics editor
+    # C.3.8 Multimedia - Audio editors
+    audacity         # audio editor
+
 
     ##### Documents and Graphics #####
-    audacity         # audio editor
-    gimp             # image editing suite
-    inkscape         # vector graphics editor
-    libreoffice-qt   # alternative to MS Office
+    libreoffice-qt   # (A.2.1) alternative to MS Office
     # Fix for kde-open5: command not found when opening links in libreoffice
     (pkgs.writeShellScriptBin "kde-open5" ''
       xdg-open "$@"
@@ -114,12 +122,12 @@
 
     ##### Others #####
     cryptor             # frontend for gocryptfs
-    wayland-protocols
-    tartube-yt-dlp      # frontend for yt-dlp, a maintained fork of youtube-dl
+    tartube-yt-dlp      # (B.4.1.2) frontend for yt-dlp, a maintained fork of youtube-dl
 
     ##### Other application package types #####
     appimage-run
-    flatpak           # handle Flatpaks
+    # TODO: See services.flatpak.enable alone is able work
+    # flatpak           # handle Flatpaks
 
     ##### Containerisation and Virtualisation #####
     qemu                   # create virtual machines
@@ -139,21 +147,8 @@
         python-telegram-bot
       ];
     in
-      python311.withPackages python-packages)
-    python311Packages.pip # package manager for python libraries
-
-    ##### Languages, package and version managers #####
-    # rbenv      # version manager tool for Ruby
-    # nodenv     # version manager tool for nodeJS
-    # yarn
-    # nodejs
-    go          # golang
-    asdf-vm
-    clang-tools_16  # for C++ LSP - clangd
-
-    ##### Other Tools #####
-
-
+      python3.withPackages python-packages)
+    python3Packages.pip # package manager for python libraries
 
     ################################################################################
     ##### Unsorted
