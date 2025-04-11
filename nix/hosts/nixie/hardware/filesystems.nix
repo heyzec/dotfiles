2361cf8702dea307d7444d0db1610c518e1b2f79
heyzec <61238538+heyzec@users.noreply.github.com>
2025-04-12 01:20:47 +0800

    nix: make ~/Documents a bind mount instead of symlink
    some apps do not treat symlinks transparently

--- nix/hosts/nixie/hardware/filesystems.nix
+++ nix/hosts/nixie/hardware/filesystems.nix
@@ -1,55 +1,79 @@
 # Refer to README.md
-{ userSettings, ... }:
 {
-  fileSystems."/" = {
-    device = "/dev/disk/by-partlabel/NixOS";
-    fsType = "btrfs";
-    options = [ "subvol=@root" ];
-  };
+  config,
+  userSettings,
+  ...
+}: {
+  ################################################################################
+  ##### System mount points
+  ################################################################################
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-partlabel/NixOS";
+      fsType = "btrfs";
+      options = ["subvol=@root"];
+    };
+    "/boot" = {
+      device = "/dev/disk/by-partlabel/EFI";
+      fsType = "vfat";
+    };
+    "/efi" = {
+      device = "/boot";
+      fsType = "none";
+      options = ["bind"];
+    };
 
-  fileSystems."/home/${userSettings.username}" = {
-    device = "/dev/disk/by-partlabel/NixOS";
-    fsType = "btrfs";
-    options = [ "subvol=@home" ];
-  };
+    ################################################################################
+    ##### User data mount points: Read-only (for backups)
+    ################################################################################
+    # These mounts are actually not RO, we can't seem to get them RO without making the
+    # next section RW
+    "/mnt/root" = {
+      device = "/dev/disk/by-partlabel/NixOS";
+      fsType = "btrfs";
+      # options = [ "subvolid=5" "ro" "nofail" ];
+      options = ["subvolid=5" "nofail"];
+    };
+    "/mnt/D" = {
+      device = "/dev/disk/by-partlabel/Data";
+      fsType = "btrfs";
+      # options = [ "subvolid=5" "ro" "compress=zstd" "nofail" ];
+      options = ["subvolid=5" "compress=zstd" "nofail"];
+    };
 
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-partlabel/EFI";
-    fsType = "vfat";
-  };
+    ################################################################################
+    ##### User data mount points: Read-write
+    ################################################################################
+    "home" = {
+      device = "/dev/disk/by-partlabel/NixOS";
+      mountPoint = "/home/${userSettings.username}";
+      fsType = "btrfs";
+      options = ["subvol=@home"];
+    };
+    "documents" = {
+      device = "${config.fileSystems."/mnt/D".mountPoint}/@data/Documents";
+      mountPoint = "${config.fileSystems."home".mountPoint}/Documents";
+      options = ["bind" "nofail"];
+    };
+    "D" = {
+      depends = ["/mnt/D"];
+      device = "/mnt/D/@data";
+      mountPoint = "/media/D";
+      fsType = "none";
+      options = ["bind" "nofail"];
+    };
 
-  fileSystems."/efi" = {
-    device = "/boot";
-    fsType = "none";
-    options = [ "bind" ];
-  };
-
-  fileSystems."/var/lib/docker/btrfs" = {
-    device = "/@/var/lib/docker/btrfs";
-    fsType = "none";
-    options = [ "bind" ];
-  };
-
-  fileSystems."/mnt/root" = {
-    device = "/dev/disk/by-partlabel/NixOS";
-    fsType = "btrfs";
-    options = [ "subvolid=5" "nofail" ];
-  };
-
-  fileSystems."/mnt/D" = {
-    device = "/dev/disk/by-partlabel/Data";
-    fsType = "btrfs";
-    options = [ "subvolid=5" "compress=zstd" "nofail" ];
-  };
-
-  fileSystems."/media/D" = {
-    depends = [ "/mnt/D" ];
-    device = "/mnt/D/@data";
-    fsType = "none";
-    options = [ "bind" "nofail" ];
+    ################################################################################
+    ##### Misc
+    ################################################################################
+    "/var/lib/docker/btrfs" = {
+      device = "/@/var/lib/docker/btrfs";
+      fsType = "none";
+      options = ["bind"];
+    };
   };
 
   swapDevices = [
-    { device = "/dev/disk/by-partlabel/Swap"; }
+    {device = "/dev/disk/by-partlabel/Swap";}
   ];
 }
