fa9bbf602ee5ba6d264129bc7483f590d0939b7f
heyzec <61238538+heyzec@users.noreply.github.com>
2025-04-22 00:06:54 +0800

    nvim: allow vscode to debug neovim configs

--- nvim/lua/init.lua
+++ nvim/lua/init.lua
@@ -1,4 +1,10 @@
 require('lazy-nvim')
+
+if init_debug then
+    require("osv").launch({port=8086, blocking=true})
+    vim.cmd.sleep(1) -- Without this, breakpoints seem to be not registered fast enough
+end
+
 require('config.keymaps')
 
 -- [[ Highlight on yank ]]
