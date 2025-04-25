554a202c96c9f36803be3021cafc8bdfc26ab754
heyzec <61238538+heyzec@users.noreply.github.com>
2025-04-25 17:18:53 +0800

    hacky nix wrapper that works to bring gcc to PATH

---
+++ nix/hosts/nixie/modules/neovim.nix
@@ -0,0 +1,63 @@
+{ pkgs, ... }: let
+  # Add non-project specific LSPs here
+  # Also see nvim/lua/plugins/mason.lua
+  extraPackages = with pkgs; [
+    # # Shell
+    # nodePackages.bash-language-server # LSP
+    # shellcheck           # linter
+
+    # # Lua for neovim
+    # lua-language-server # LSP
+
+    # # Nix
+    # cargo gccgo  # Needed by mason-tool-installer to install nil
+    # nil          # LSP for nix
+    # nixpkgs-fmt  # External formatter used by nil
+
+    # # Misc
+    # vscode-langservers-extracted # css, eslint, html, json
+    # yaml-language-server
+
+    # No C compiler found! "cc", "gcc", "clang", "cl", "zig" are not executable.
+    # We need a compiler
+    gcc
+  ];
+in {
+  programs = {
+    # A little hacky because
+    # 1. we are trying to force the output of our ...pkgs.symlinkJoin {}
+    #   to fit the expectations of pkgs.neovim-unwrapped. We are
+    # 2. We are exposed to API changes of the wrapper, if people add extra fields
+    # 3. Our object may no longer behave as expected with regards to module options, e.g.
+    # with NodeJs no longer work (untested)
+    # Consider using environment.systemPackages instead
+    neovim = {
+      # package = pkgs.neovim-unwrapped;
+      package = pkgs.symlinkJoin {
+        name = "neovim-unwrapped-wrapped";
+        meta = pkgs.neovim-unwrapped.meta;
+        lua = pkgs.neovim-unwrapped.lua;
+        paths = [ pkgs.neovim-unwrapped ];
+        nativeBuildInputs = [ pkgs.makeBinaryWrapper ];
+        postBuild = let
+          # path = builtins.trace (pkgs.lib.makeBinPath extraPackages);
+          path = (pkgs.lib.makeBinPath extraPackages);
+        in ''
+          wrapProgram $out/bin/nvim \
+            --prefix PATH : ${path}
+        '';
+      };
+      # package = pkgs.neovim-unwrapped.overrideAttrs {
+      #   foo = "bar";
+      # };
+      enable = true;
+      defaultEditor = true;
+
+      # For mason to install LSPs using npm
+      withNodeJs = true;
+
+      # Configure vimrc using non-nix config file
+      # configure = {};
+    };
+  };
+}
