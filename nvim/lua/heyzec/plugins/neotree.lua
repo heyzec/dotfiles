cfc315874683a3ef2e7d22a5d5a5aa63baa43db7
heyzec <61238538+heyzec@users.noreply.github.com>
2025-04-27 02:01:39 +0800

    make dogfooding more comfortable

--- nvim/lua/heyzec/plugins/neotree.lua
+++ nvim/lua/heyzec/plugins/neotree.lua
@@ -6,4 +6,16 @@ return {
     'MunifTanjim/nui.nvim',
     -- -- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
   },
+  keys = {
+    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+  },
+  opts = {
+    filesystem = {
+      window = {
+        mappings = {
+          ['\\'] = 'close_window',
+        },
+      },
+    },
+  },
 }
