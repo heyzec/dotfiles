039aacfc42c211f48c60f316fcb36c44835b3496
heyzec <61238538+heyzec@users.noreply.github.com>
2025-05-04 00:48:37 +0800

    better completion

--- nvim/lua/heyzec/plugins/completion.lua
+++ nvim/lua/heyzec/plugins/completion.lua
@@ -1,45 +1,44 @@
 return {
-  {
-    'saghen/blink.cmp',
-    event = 'VimEnter',
-    --- @module 'blink.cmp'
-    --- @type blink.cmp.Config
-    opts = {
-      keymap = {
-        preset = 'default',
-      },
-      completion = {
-        menu = {
-          border = 'rounded',
-          draw = {
-            columns = {
-              { 'label' },
-              { 'label_description', 'kind_icon', gap = 10 },
-              { 'kind' },
-            },
-          },
-        },
-        documentation = {
-          auto_show = true,
-          window = { border = 'rounded' },
+  'saghen/blink.cmp',
+  enabled = true,
+  event = 'VimEnter',
+  --- @module 'blink.cmp'
+  --- @type blink.cmp.Config
+  opts = {
+    keymap = {
+      preset = 'default',
+      ['<CR>'] = { 'accept', 'fallback' },
+    },
+    completion = {
+      list = { selection = { preselect = false, auto_insert = false } },
+      menu = {
+        border = 'rounded',
+        draw = {
+          gap = 1,
+          columns = { { 'kind_icon' }, { 'label', 'label_description', 'source_name', gap = 4 } },
+          treesitter = { 'lsp' }, -- use treesitter to highlight LSP sources
         },
       },
-      -- show function signature while typing arguments
-      signature = {
-        enabled = true,
+      documentation = {
+        auto_show = true,
         window = { border = 'rounded' },
       },
-      cmdline = {
-        keymap = {
-          ['<Tab>'] = { 'show', 'select_next' },
-          ['<S-Tab>'] = { 'select_prev' },
-          ['<Down>'] = { 'select_next', 'fallback' },
-          ['<Up>'] = { 'select_prev', 'fallback' },
-        },
-        completion = {
-          menu = { auto_show = true },
-          list = { selection = { preselect = false } },
-        },
+    },
+    -- show function signature while typing arguments
+    signature = {
+      enabled = true,
+      window = { border = 'rounded' },
+    },
+    cmdline = {
+      keymap = {
+        ['<Tab>'] = { 'show', 'select_next' },
+        ['<S-Tab>'] = { 'select_prev' },
+        ['<Down>'] = { 'select_next', 'fallback' },
+        ['<Up>'] = { 'select_prev', 'fallback' },
+      },
+      completion = {
+        list = { selection = { preselect = false, auto_insert = true } },
+        menu = { auto_show = true },
       },
     },
   },
