eca3143cecf16afede313090347a8c674445cee6
heyzec <61238538+heyzec@users.noreply.github.com>
2025-06-30 02:47:57 +0800

    use niri (GROWING)

--- nix/hosts/devpad/modules/hyprland.nix
+++ nix/hosts/devpad/modules/hyprland.nix
@@ -1,6 +1,6 @@
-{ pkgs, ... }:
-{
+{pkgs, ...}: {
   # Refer to home-manager for entry-point to configurations
+  programs.niri.enable = true;
   programs.hyprland = {
     enable = true;
     xwayland.enable = true;
