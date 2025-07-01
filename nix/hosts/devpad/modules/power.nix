caff1ee7a1895bfcdd98263660cc245f151bf445
heyzec <61238538+heyzec@users.noreply.github.com>
2025-07-01 10:43:55 +0800

    screw black screens

--- nix/hosts/devpad/modules/power.nix
+++ nix/hosts/devpad/modules/power.nix
@@ -1,10 +1,10 @@
 {
   heyzec.power = {
-    enable = true;
+    enable = false;
 
-    blankAfter = 10 * 60;           # 10 minutes
-    sleepAfter = 15 * 60;           # 15 minutes
-    hibernateAfter = 1 * 60 * 60;   # 1 hour
+    blankAfter = 10 * 60; # 10 minutes
+    sleepAfter = 15 * 60; # 15 minutes
+    hibernateAfter = 1 * 60 * 60; # 1 hour
 
     commandBlankScreen = "hyprctl dispatch dpms off";
     commandUnblankScreen = "hyprctl dispatch dpms on";
