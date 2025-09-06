a45fdff000b0cafd13e514d22a750d1729f7e7ce
heyzec <61238538+heyzec@users.noreply.github.com>
2025-09-06 16:07:10 +0800

    desktop(swhkd): update config and improve packaging

--- nix/overlays/swhkd.nix
+++ nix/overlays/swhkd.nix
@@ -4,60 +4,51 @@
   ...
 }: {
   imports = [
-    (lib.heyzec.overrideNixpkgs {
-      inherit pkgs;
-      url = "https://github.com/mibmo/nixpkgs/archive/03cccb60c220ba25a0aa7be7221204156fe29bc6.tar.gz";
-      sha256 = "17ffvjv65r8zy5vdva3f29r2bbrsm4rmiyl8f5mqsbwkx5g7crw6";
-      names = ["swhkd"];
-    })
+    {
+      nixpkgs.overlays = [
+        (final: prev: {
+          swhkd = pkgs.rustPlatform.buildRustPackage rec {
+            pname = "swhkd";
+            version = "c5c4071459a6465a3743a8bb5bb990e27cdf315b";
+
+            src = pkgs.fetchFromGitHub {
+              owner = "waycrate";
+              repo = "swhkd";
+              rev = version;
+              hash = "sha256-Tv9+UBDBuRD3equ2XNmyt3Fm1+9DxkRzqV4M7PWnBLA=";
+            };
+            patches = [./swhkd.patch];
+
+            cargoHash = "sha256-LBbmFyddyw7vV5voctXq3L4U3Ddbh428j5XbI+td/dg=";
+
+            buildInputs = with pkgs; [
+              eudev
+            ];
+
+            nativeBuildInputs = with pkgs; [
+              scdoc
+              pkg-config
+            ];
+
+            buildFeatures = [
+              # See https://github.com/waycrate/swhkd/issues/310
+              "no_rfkill"
+            ];
+
+            meta = with lib; {
+              description = "Display protocol-independent hotkey daemon";
+              longDescription = ''
+                A display protocol-independent hotkey daemon made in Rust.
+                Uses an easy-to-use configuration system inspired by sxhkd, so you can easily add or remove hotkeys.
+                Attempts to be a drop-in replacement of sxhkd, meaning your sxhkd config file should be compatible with swhkd.
+              '';
+              license = licenses.bsd2;
+              maintainers = with maintainers; [mib];
+              platforms = platforms.linux;
+            };
+          };
+        })
+      ];
+    }
   ];
 }
-# This overlay is based off a closed PR on the Nixpkgs repository.
-# In case the commit is lost, the contents of the PR is included below.
-# From 03cccb60c220ba25a0aa7be7221204156fe29bc6 Mon Sep 17 00:00:00 2001
-# From: mib <mib@mib.dev>
-# Date: Sat, 16 Sep 2023 00:06:07 +0200
-# Subject: [PATCH] swhkd: init at 1.2.2
-#
-# ---
-#  pkgs/by-name/sw/swhkd/package.nix | 30 ++++++++++++++++++++++++++++++
-#  1 file changed, 30 insertions(+)
-#  create mode 100644 pkgs/by-name/sw/swhkd/package.nix
-#
-# diff --git a/pkgs/by-name/sw/swhkd/package.nix b/pkgs/by-name/sw/swhkd/package.nix
-# new file mode 100644
-# index 0000000000000..e3aa86ab51397
-# --- /dev/null
-# +++ b/pkgs/by-name/sw/swhkd/package.nix
-# @@ -0,0 +1,30 @@
-# +{ lib
-# +, fetchFromGitHub
-# +, rustPlatform
-# +}:
-# +
-# +rustPlatform.buildRustPackage rec {
-# +  pname = "swhkd";
-# +  version = "1.2.2";
-# +
-# +  src = fetchFromGitHub {
-# +    owner = "waycrate";
-# +    repo  = "swhkd";
-# +    rev = version;
-# +    hash = "sha256-mpE+//a44wwraCCpBTnWXslLROF2dSIcv/kdpxHLD4M=";
-# +  };
-# +
-# +  cargoHash = "sha256-H14YwK4Ow86UxVXoclCjk2xYtu8L/44zkzf9gpveAh8=";
-# +
-# +  meta = with lib; {
-# +    description = "Display protocol-independent hotkey daemon";
-# +    longDescription = ''
-# +      A display protocol-independent hotkey daemon made in Rust.
-# +      Uses an easy-to-use configuration system inspired by sxhkd, so you can easily add or remove hotkeys.
-# +      Attempts to be a drop-in replacement of sxhkd, meaning your sxhkd config file should be compatible with swhkd.
-# +    '';
-# +    license = licenses.bsd2;
-# +    maintainers = with maintainers; [ mib ];
-# +    platforms = platforms.linux;
-# +  };
-# +}
-
