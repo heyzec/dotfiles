13387fd1ac1794db39aca2d084e38c9d1f26287a
heyzec <61238538+heyzec@users.noreply.github.com>
2025-09-16 20:47:35 +0800

    nix: add printer (Epson-L360)

--- nix/hosts/devpad/modules/printing.nix
+++ nix/hosts/devpad/modules/printing.nix
@@ -1,19 +1,42 @@
-{ pkgs, ... }:
-
-{
-  # PRINTERS
+# peripherals.nix
+{pkgs, ...}: {
+  ################################################################################
+  ##### Printers
+  ################################################################################
   services.printing.enable = true;
+  # services.printing.logLevel = "debug";
+  hardware.printers = {
+    ensureDefaultPrinter = "Epson-L360";
+    ensurePrinters = [
+      {
+        name = "Epson-L360";
+        # To find device URI, use `lpinfo -v`
+        deviceUri = "usb://EPSON/L360%20Series?serial=5647444B3331343958&interface=1";
+        location = "home";
+        model = "gutenprint.5.3://escp2-l310/expert";
+        ppdOptions = {
+          PageSize = "A4";
+        };
+      }
+    ];
+  };
+  services.printing.drivers = with pkgs; [
+    gutenprint
+    gutenprintBin
+  ];
 
-  # SCANNERS
+  ################################################################################
+  ##### Scanners
+  ################################################################################
   hardware.sane = {
     enable = true;
 
     drivers.scanSnap.enable = true;
     extraBackends = with pkgs; [
       utsushi # for some Epson scanners
-      sane-airscan         # for "driverless" scanning
+      sane-airscan # for "driverless" scanning
     ];
   };
   # use epsonscan nexttime
-  services.udev.packages = [ pkgs.utsushi ];
+  services.udev.packages = [pkgs.utsushi];
 }
