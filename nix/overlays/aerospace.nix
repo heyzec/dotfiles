abf161d640e0770819d357d9d58c72ab21939fda
heyzec <61238538+heyzec@users.noreply.github.com>
2025-10-11 14:01:12 +0800

    a commit for aerospace

---
+++ nix/overlays/aerospace.nix
@@ -0,0 +1,15 @@
+# https://github.com/nix-darwin/nix-darwin/issues/1041#issuecomment-2481680350
+{
+  nixpkgs.overlays = [
+    (self: super: {
+      aerospace = super.aerospace.overrideAttrs (old: rec {
+        version = "0.19.2-Beta";
+
+        src = super.fetchzip {
+          url = "https://github.com/nikitabobko/AeroSpace/releases/download/v${version}/AeroSpace-v${version}.zip";
+          hash = "sha256-6RyGw84GhGwULzN0ObjsB3nzRu1HYQS/qoCvzVWOYWQ=";
+        };
+      });
+    })
+  ];
+}
