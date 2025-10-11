316a0e38b9ebfc698e40ee99c8b78f12fd98c457
heyzec <61238538+heyzec@users.noreply.github.com>
2025-10-11 13:01:08 +0800

    pinentry

--- nix/home/linux/hyprland.nix
+++ nix/home/linux/hyprland.nix
@@ -38,6 +38,11 @@ in {
   # };
   # services.ssh-agent.enable = true;
   services.gpg-agent.enable = true;
+  # ```
+  #   trace: warning: The option `services.gpg-agent.pinentryPackage' defined in `/nix/store/wwazwjdk02kl7wmz9kvgw9km6hxh7h0s-source/nix/home/linux/hyprland.nix' has been renamed to `services.gpg-agent.pinentry.package'.
+  #
+  #   `
+  services.gpg-agent.pinentryPackage = pkgs.pinentry-gnome3;
   services.gpg-agent.enableSshSupport = true;
 
   systemd.user.services.indicator-sound-switcher = {
