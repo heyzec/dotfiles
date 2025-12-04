8523a49bb69a9d19d3149e9bf79cd96732b39d8e
heyzec <61238538+heyzec@users.noreply.github.com>
2025-12-05 01:54:14 +0800

    nix(darwin): set macOS related configs

--- nix/hosts/darwin/apple.nix
+++ nix/hosts/darwin/apple.nix
@@ -3,9 +3,29 @@
     # Only ask for password if running for longer than 10 seconds
     screensaver.askForPasswordDelay = 10;
 
+    dock = {
+      autohide = true;
+
+      # https://apple.stackexchange.com/a/70598
+      autohide-delay = 0.0;
+      autohide-time-modifier = 0.4;
+
+      # https://nikitabobko.github.io/AeroSpace/guide#a-note-on-mission-control
+      expose-group-apps = true;
+
+      # Disable hot corner action for bottom-right (defaults to Quick Note)
+      wvous-br-corner = 1;
+    };
+
+    WindowManager = {
+      # Disable click wallpaper to reveal desktop
+      EnableStandardClickToShowDesktop = false;
+    };
+
+    # defaults write -g ...
     NSGlobalDomain = {
       InitialKeyRepeat = 25;
-      KeyRepeat = 3;  # Smaller is faster
+      KeyRepeat = 3; # Smaller is faster
 
       # Disable "smart" autocorrect features
       NSAutomaticCapitalizationEnabled = false;
@@ -13,15 +33,30 @@
       NSAutomaticPeriodSubstitutionEnabled = false;
       NSAutomaticQuoteSubstitutionEnabled = false;
       NSAutomaticSpellingCorrectionEnabled = false;
+
+      # Disable special character popup
+      ApplePressAndHoldEnabled = false;
+
+      # Don't accidentally trigger Look Up feature when pressing harder on trackpad
+      "com.apple.trackpad.forceClick" = false;
     };
 
+    ".GlobalPreferences" = {
+      "com.apple.mouse.scaling" = 1.0; # the setting of this value seems glitchy
+    };
+
+    # CustomUserPreferences is like an escape hatch, use it only if nix-darwin has no related option
     CustomUserPreferences = {
-      "com.apple.dock" = {
-        autohide = true;
-        # https://apple.stackexchange.com/a/70598
-        autohide-delay = 0;
-        autohide-time-modifier = 0.4;
+      # NSGlobalDomain = {};
+      # See https://apple.stackexchange.com/questions/365048/disable-dictation-from-command-line
+      "com.apple.HIToolbox" = {
+        AppleDictationAutoEnable = false;
       };
     };
+  }; # system.defaults
+
+  security.pam.services.sudo_local = {
+    touchIdAuth = true; # Use Touch ID for sudo in terminals
+    reattach = true; # Fixes Touch ID not working inside tmux
   };
 }
