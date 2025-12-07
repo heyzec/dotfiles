de9514410939f848ff9073d57fa0e535a65a3569
heyzec <61238538+heyzec@users.noreply.github.com>
2025-12-08 01:37:05 +0800

    TODO: nix modules refactor

--- nix/modules/default.nix
+++ nix/modules/default.nix
@@ -1,3 +1,7 @@
+# List of locations that can import modules in this folder
+# - nix/home/default.nix (imports selected modules)
+# - nix/utils.nix (mkNixosSystem imports this file)
+
 { lib, ... }:
 {
   imports = lib.heyzec.umport {
