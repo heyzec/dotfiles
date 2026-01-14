39c5e69fd8d6e3756e6987b269f24f076b100d9d
heyzec <61238538+heyzec@users.noreply.github.com>
2026-01-15 00:51:46 +0800

    vscode: update extensions

--- nix/home/linux/vscode.nix
+++ nix/home/linux/vscode.nix
@@ -2,15 +2,54 @@
   programs.vscode = {
     enable = true;
     # Whether extensions can be modified by Visual Studio Code
-    mutableExtensionsDir = true;
+    mutableExtensionsDir = false;
 
     # Until we are able to declaratively install extensions for each profile, install all extensions via VSCode
     # This is because once ~/.vscode/extensions/extensions.json is generated, it will no longer be updated by home-manager
-    # extensions = with pkgs.vscode-extensions; [
-    #   asvetliakov.vscode-neovim
-    #   waderyan.gitblame
-    #   usernamehw.errorlens
-    #   esbenp.prettier-vscode
-    # ];
+    profiles = let
+      defaultExtensions = with pkgs.vscode-extensions; [
+        asvetliakov.vscode-neovim
+        waderyan.gitblame
+        usernamehw.errorlens
+        esbenp.prettier-vscode
+        mkhl.direnv
+        (pkgs.vscode-utils.buildVscodeMarketplaceExtension {
+          # JoshMu.periscope
+          mktplcRef = {
+            name = "periscope";
+            publisher = "JoshMu";
+            version = "1.11.1";
+            hash = "sha256-Eed3vcmwbxThd/MmImgYzQHhkUe2O0OJszSUHPsknlc=";
+          };
+        })
+      ];
+      extendWith = moreExtensions: defaultExtensions ++ moreExtensions;
+    in {
+      "default" = {
+        extensions = defaultExtensions;
+      };
+      "Python" = {
+        extensions = extendWith (with pkgs.vscode-extensions; [
+          ms-python.python
+          ms-python.vscode-pylance
+          ms-python.debugpy
+        ]);
+      };
+      "TypeScript" = {
+        extensions = extendWith (with pkgs.vscode-extensions; [
+          yoavbls.pretty-ts-errors
+        ]);
+      };
+      "C/C++" = {
+        extensions = extendWith (with pkgs.vscode-extensions; [
+          llvm-vs-code-extensions.vscode-clangd
+        ]);
+      };
+      "Go" = {
+        extensions = extendWith (with pkgs.vscode-extensions; [
+          golang.go
+        ]);
+      };
+    };
   };
 }
