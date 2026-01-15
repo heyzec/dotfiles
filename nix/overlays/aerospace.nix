a62663bf419c379db79e234039029765788f62e7
Tiang Hui Zheng <huizheng.tiang@shopee.com>
2026-01-15 22:40:05 +0800

    nix: aerospace config

--- nix/overlays/aerospace.nix
+++ nix/overlays/aerospace.nix
@@ -1,14 +1,10 @@
-{
-  nixpkgs.overlays = [
-    (self: super: {
-      aerospace = super.aerospace.overrideAttrs (old: rec {
-        version = "0.19.2-Beta";
+final: prev: {
+  aerospace = prev.aerospace.overrideAttrs (old: rec {
+    version = "0.19.2-Beta";
 
-        src = super.fetchzip {
-          url = "https://github.com/nikitabobko/AeroSpace/releases/download/v${version}/AeroSpace-v${version}.zip";
-          hash = "sha256-6RyGw84GhGwULzN0ObjsB3nzRu1HYQS/qoCvzVWOYWQ=";
-        };
-      });
-    })
-  ];
+    src = prev.fetchzip {
+      url = "https://github.com/nikitabobko/AeroSpace/releases/download/v${version}/AeroSpace-v${version}.zip";
+      hash = "sha256-6RyGw84GhGwULzN0ObjsB3nzRu1HYQS/qoCvzVWOYWQ=";
+    };
+  });
 }
