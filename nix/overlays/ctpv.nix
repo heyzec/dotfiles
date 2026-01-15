f3485df1cc674f735a344048689861c125e41ac0
Tiang Hui Zheng <huizheng.tiang@shopee.com>
2026-01-15 10:05:04 +0800

    remove ueberzug from ctpv on darwin (PR to nixpkgs)

--- nix/overlays/ctpv.nix
+++ nix/overlays/ctpv.nix
@@ -8,5 +8,27 @@ in {
       if final.stdenv.isAarch64
       then ["-fsigned-char"]
       else [];
+
+    preFixup = ''
+      wrapProgram $out/bin/ctpv \
+        --prefix PATH ":" "${
+        prev.lib.makeBinPath [
+          prev.atool # for archive files
+          prev.bat
+          prev.chafa # for image files on Wayland
+          prev.delta # for diff files
+          prev.ffmpeg
+          prev.ffmpegthumbnailer
+          prev.fontforge
+          prev.glow # for markdown files
+          prev.imagemagick
+          prev.jq # for json files
+          prev.poppler-utils # for pdf files
+          # prev.ueberzug # for image files on X11
+        ]
+      }";
+    '';
+
+    meta.platforms = with prev.lib; platforms.linux ++ platforms.darwin;
   });
 }
