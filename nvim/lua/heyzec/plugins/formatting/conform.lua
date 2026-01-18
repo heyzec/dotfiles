45e5ce9bcfdf3c451afa7894602fd1c574b9bf6b
heyzec <61238538+heyzec@users.noreply.github.com>
2026-01-18 15:35:58 +0800

    nvim: update configs

--- nvim/lua/heyzec/plugins/formatting/conform.lua
+++ nvim/lua/heyzec/plugins/formatting/conform.lua
@@ -6,6 +6,9 @@ return {
   ---@module 'conform'
   ---@type conform.setupOpts
   opts = {
+    default_format_opts = {
+      lsp_format = 'fallback',
+    },
     formatters_by_ft = require('heyzec.utils.tooling').get_formatters_config(),
   },
 }
