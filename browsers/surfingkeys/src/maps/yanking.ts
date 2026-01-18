989d36b22943552c39ab08d40e7d523b2f77a8f6
heyzec <61238538+heyzec@users.noreply.github.com>
2026-01-19 01:28:29 +0800

    surfingkeys: dump

--- browsers/surfingkeys/src/maps/yanking.ts
+++ browsers/surfingkeys/src/maps/yanking.ts
@@ -15,3 +15,37 @@ api.mapkey("yy", "Copy link (Wikipedia)", copyWithoutParameters, {
 api.mapkey("yy", "Copy link (TikTok)", copyWithoutParameters, {
   domain: /www\.tiktok\.com/,
 });
+
+// Confluence
+api.mapkey(
+  "yy",
+  "Copy link (Confluence)",
+  function () {
+    // Use shortlink instead (which is a permalink)
+    const link = document.querySelector('link[rel="shortlink"]').href;
+    api.Clipboard.write(link);
+  },
+  { domain: /confluence\..+\..+/ },
+);
+
+// Google Maps coordinates reversed engineered
+api.mapkey(
+  "yy",
+  "Copy link (Google Maps)",
+  () => {
+    if (!window.location.href.startsWith("https://www.google.com/maps/place")) {
+      console.log("Not a place URL");
+      return;
+    }
+    const script = document.head.querySelector("script:not([src])");
+    let text = script.textContent;
+    let match = text.match(/window\.ES5DGURL='(.+?)data/);
+    text = match[1];
+    match = text.match(/\/maps\/place\/(.+?)\/@([\d.]+,[\d.]+),.+?\//);
+    coords = match[2];
+    api.Clipboard.write(coords);
+  },
+  {
+    domain: /www\.google\.com/,
+  },
+);
