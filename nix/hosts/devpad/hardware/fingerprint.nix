3168114128291493274cefab9163a8ab6806de8c
heyzec <61238538+heyzec@users.noreply.github.com>
2026-02-27 01:44:01 +0800

    nix+hyprlock: enable fingerprint for hyprlock

--- nix/hosts/devpad/hardware/fingerprint.nix
+++ nix/hosts/devpad/hardware/fingerprint.nix
@@ -1,10 +1,12 @@
-{lib, ...}: {
-  # nixos-hardware already enables fprintd by default
-  # Note: enroll properly
-  # https://discourse.nixos.org/t/nixos-unstable-fprintd-fingerprint-reader-issues/33273/2
+{
+  # enroll fingerprint properly with fprintd-enroll
+  # see also: https://discourse.nixos.org/t/nixos-unstable-fprintd-fingerprint-reader-issues/33273/2
 
-  services.fprintd.enable = lib.mkForce false;
+  services.fprintd.enable = true; # also enabled by nixos-hardware for Dell XPS 13 9310
+
+  # Since the fingerprint reader is unreliable and PAM currently doesn't support parallel auth,
+  # enabling it by default can lock us out. Therefore, we disable fingerprint by default.
+  # (this is not an issue for hyprlock)
   security.pam.services.login.fprintAuth = false;
-  security.pam.services.hyprlock.fprintAuth = false;
   security.pam.services.sudo.fprintAuth = false;
 }
