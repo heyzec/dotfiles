c9e39f7a30f7b9e69338e81739ed69e74348d6ec
heyzec <61238538+heyzec@users.noreply.github.com>
2026-03-29 00:30:32 +0800

    nix: make sudo show pwfeedback

--- nix/hosts/devpad/modules/security.nix
+++ nix/hosts/devpad/modules/security.nix
@@ -2,6 +2,10 @@
   security = {
     polkit.enable = true;
     pam.services.hyprlock = {};
+
+    sudo.extraConfig = ''
+      Defaults pwfeedback
+    '';
   };
 
   programs.hyprlock.enable = true;
