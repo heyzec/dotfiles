fb8b0abb5f765979d5923f6aab3eeb5f51d93e44
heyzec <61238538+heyzec@users.noreply.github.com>
2026-05-05 09:01:01 +0800

    lf: fix quoting issue and ctpv building issues

---
+++ nix/overlays/ctpv.nix
@@ -0,0 +1,35 @@
+# ctpv (used within lf) is broken on homelab
+# See https://github.com/NikitaIvanovV/ctpv/issues/101
+# Remove after https://github.com/NixOS/nixpkgs/pull/481650 merged
+final: prev: let
+  name = "ctpv";
+in {
+  ${name} = prev.${name}.overrideAttrs (finalAttrs: prevAttrs: {
+    CFLAGS =
+      if final.stdenv.isAarch64
+      then ["-fsigned-char"]
+      else [];
+
+    preFixup = ''
+      wrapProgram $out/bin/ctpv \
+        --prefix PATH ":" "${
+        prev.lib.makeBinPath [
+          prev.atool # for archive files
+          prev.bat
+          prev.chafa # for image files on Wayland
+          prev.delta # for diff files
+          prev.ffmpeg
+          prev.ffmpegthumbnailer
+          prev.fontforge
+          prev.glow # for markdown files
+          prev.imagemagick
+          prev.jq # for json files
+          prev.poppler-utils # for pdf files
+          # prev.ueberzug # for image files on X11
+        ]
+      }";
+    '';
+
+    meta.platforms = with prev.lib; platforms.linux ++ platforms.darwin;
+  });
+}
