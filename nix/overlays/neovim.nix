b5973d01e2f45cff9743208fc6974e5601d4669b
heyzec <61238538+heyzec@users.noreply.github.com>
2026-05-07 20:34:47 +0800

    nvim: use stable v0.12 and bump plugins

--- nix/overlays/neovim.nix
+++ nix/overlays/neovim.nix
@@ -1,13 +1,13 @@
 # For https://github.com/neovim/neovim/issues/33869
 final: prev: {
   neovim-unwrapped = prev.neovim-unwrapped.overrideAttrs (old: rec {
-    version = "v0.12.0-dev";
+    version = "v0.12.2";
 
     src = final.fetchFromGitHub {
       owner = "neovim";
       repo = "neovim";
-      rev = "23bf4c0531acef4e8252f4db13fcd90ad5aa91bf";
-      hash = "sha256-FJZZRSj9YsWjEuXqGQEE00Uxc1pIp7lon6cYzEU3yKg=";
+      rev = "${version}";
+      hash = "sha256-V+jZiNv0SvG/GOOUPzmBkOQGrnrN3UW2BY2n9NxP2Eg=";
     };
   });
 }
