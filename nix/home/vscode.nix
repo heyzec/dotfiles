7a24d85db90f5d085721803e4bc7b15331590ad5
heyzec <61238538+heyzec@users.noreply.github.com>
2026-06-02 21:44:19 +0800

    vscode: use nix to organise settings

--- nix/home/vscode.nix
+++ nix/home/vscode.nix
@@ -1,55 +1,88 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  config,
+  userSettings,
+  ...
+}: let
+  username = userSettings.username;
+  root = "/Users/${username}/dotfiles/vscode";
+
+  # Define extensions that are not in nixpkgs here
+  extra = {
+    periscope = pkgs.vscode-utils.buildVscodeMarketplaceExtension {
+      # JoshMu.periscope
+      mktplcRef = {
+        name = "periscope";
+        publisher = "JoshMu";
+        version = "1.11.1";
+        hash = "sha256-Eed3vcmwbxThd/MmImgYzQHhkUe2O0OJszSUHPsknlc=";
+      };
+    };
+    inherit-profile = pkgs.vscode-utils.buildVscodeMarketplaceExtension {
+      # alexthomson.inherit-profile
+      mktplcRef = {
+        name = "inherit-profile";
+        publisher = "alexthomson";
+        version = "0.6.0";
+        hash = "sha256-mKic9Sjarpm6JPSzzTzPUxL4knq6YEVA1QwtpYE48EI=";
+      };
+    };
+    vscode-pbkit = pkgs.vscode-utils.buildVscodeMarketplaceExtension {
+      # pbkit.vscode-pbkit
+      mktplcRef = {
+        name = "vscode-pbkit";
+        publisher = "pbkit";
+        version = "0.0.8";
+        hash = "sha256-xM6yKLx9rntZZO0VMGrpyvIFhNAV5pFWIFrUqKglZyI=";
+      };
+    };
+  };
+
+  # List of extensions used in all profiles
+  defaultExtensions = with pkgs.vscode-extensions; [
+    asvetliakov.vscode-neovim
+    waderyan.gitblame
+    usernamehw.errorlens
+    esbenp.prettier-vscode
+    mkhl.direnv
+    extra.periscope
+  ];
+
+  mkProfile = profile: extensions: {
+    extensions =
+      defaultExtensions
+      ++ extensions
+      ++
+      # Workaround until https://github.com/microsoft/vscode/issues/188612 fixed
+      [extra.inherit-profile];
+    keybindings = config.lib.file.mkOutOfStoreSymlink "${root}/keybindings.jsonc";
+    userSettings = config.lib.file.mkOutOfStoreSymlink "${root}/settings-${profile}.jsonc";
+  };
+in {
   programs.vscode = {
     enable = true;
-    # Whether extensions can be modified by Visual Studio Code
-    mutableExtensionsDir = false;
+    mutableExtensionsDir = false; # Whether extensions can be modified by Visual Studio Code
 
-    # Until we are able to declaratively install extensions for each profile, install all extensions via VSCode
-    # This is because once ~/.vscode/extensions/extensions.json is generated, it will no longer be updated by home-manager
-    profiles = let
-      defaultExtensions = with pkgs.vscode-extensions; [
-        asvetliakov.vscode-neovim
-        waderyan.gitblame
-        usernamehw.errorlens
-        esbenp.prettier-vscode
-        mkhl.direnv
-        (pkgs.vscode-utils.buildVscodeMarketplaceExtension {
-          # JoshMu.periscope
-          mktplcRef = {
-            name = "periscope";
-            publisher = "JoshMu";
-            version = "1.11.1";
-            hash = "sha256-Eed3vcmwbxThd/MmImgYzQHhkUe2O0OJszSUHPsknlc=";
-          };
-        })
-      ];
-      extendWith = moreExtensions: defaultExtensions ++ moreExtensions;
-    in {
+    # Ensure for each non-default profile, ${root}/settings-<profile>.jsonc has been created
+    profiles = {
       "default" = {
         extensions = defaultExtensions;
       };
-      "Python" = {
-        extensions = extendWith (with pkgs.vscode-extensions; [
-          ms-python.python
-          ms-python.vscode-pylance
-          ms-python.debugpy
-        ]);
-      };
-      "TypeScript" = {
-        extensions = extendWith (with pkgs.vscode-extensions; [
-          yoavbls.pretty-ts-errors
-        ]);
-      };
-      "C/C++" = {
-        extensions = extendWith (with pkgs.vscode-extensions; [
-          llvm-vs-code-extensions.vscode-clangd
-        ]);
-      };
-      "Go" = {
-        extensions = extendWith (with pkgs.vscode-extensions; [
-          golang.go
-        ]);
-      };
+      # "Python" = mkProfile "Python" (with pkgs.vscode-extensions; [
+      #   ms-python.python
+      #   ms-python.vscode-pylance
+      #   ms-python.debugpy
+      # ]);
+      # "TypeScript" = mkProfile "TypeScript" (with pkgs.vscode-extensions; [
+      #   yoavbls.pretty-ts-errors
+      # ]);
+      # "C" = mkProfile "C" (with pkgs.vscode-extensions; [
+      #   llvm-vs-code-extensions.vscode-clangd
+      # ]);
+      "Go" = mkProfile "Go" (with pkgs.vscode-extensions; [
+        golang.go
+        extra.vscode-pbkit
+      ]);
     };
   };
 }
